9ccba96289647f83b2cf20c8f6cdf2b1d0ec0b7e
Parihar Naresh Singh <pariharsoftwdev@gmail.com>
2026-01-19 19:35:55 +0530

    all the stuff

---
+++ rustpro/projects/project7/src/main.rs
@@ -0,0 +1,130 @@
+
+/*
+Define a Food struct with a single `name` field
+set to a String. Derive a Debug implementation.
+ 
+Define a Restaurant struct with a `reservations` field
+set to a u32 and a `has_mice_infestation` field set to
+a bool. Derive a Debug implementation.
+ 
+Define a `chef_special` method on the Restaurant.
+The method will return the restaurant's famous
+dish. It should return an Option containing a Food
+struct.
+ 
+If the restaurant has a mice infestation, return the
+None variant. There is no chef special!
+ 
+If the restaurant has less than 12 reservations, return
+a Food instance with a name of "Uni Sashimi" wrapped in
+the Some variant. If it has 12 or more reservations,
+return a Food instance with a name of "Strip Steak"
+instead, also wrapped in the Some variant.
+ 
+Define a `deliver_burger` method on the Restaurant.
+It should accept an `address` string slice; it will
+represent where to deliver the order. It should
+return a Result type where the Ok variant holds a Food
+struct and the Err variant holds a String.
+ 
+If the restaurant has a mice infestation, return the
+Err variant containing a String of "Sorry, we have a
+mice problem".
+ 
+If the user's address is an empty string, return the Err
+variant with a String of "No delivery address specified".
+HINT: You can use the `is_empty` method on a string to check
+if it has 0 characters.
+https://doc.rust-lang.org/std/string/struct.String.html#method.is_empty
+ 
+Otherwise, the delivery is good to go! Return the Ok
+variant containing a Food struct with a `name` of "Burger".
+ 
+In the `main` function, create a `Restaurant` instance
+with 11 reservations and a mice infestation.
+ 
+Invoke the `chef_special` method and print out its return
+value. It should be the None variant.
+ 
+Invoke the `deliver_burger` method with an argument of "123
+Elm Street" and print out its return value. It should be
+the Err variant.
+ 
+Create another `Restaurant` instance with 15 reservations
+and no mice infestation.
+ 
+Invoke the `chef_special` method and print out its return
+ value. It should be the Some variant with a "Strip Steak".
+ 
+Invoke the `deliver_burger` method with an argument of an
+empty address. Print out its return value. It should be the
+Err variant.
+ 
+Invoke the `deliver_burger` method again with an argument
+of a valid address. Print out its return value. It should
+be the Ok variant nesting a Food struct with a `name` of
+"Burger".
+*/
+#[derive(Debug)]
+struct Food{
+    name:String,
+}
+
+#[derive(Debug)]
+struct Restaurant{
+    reservations:u32,
+    has_mice_infestation:bool,
+}
+
+impl Restaurant{
+    fn chef_special(&self) -> Option<Food> {
+        if(self.has_mice_infestation){
+            return Option::None;
+        }
+
+        if(self.reservations<12){
+            return Option::Some(Food{
+                name:String::from("Unin Sishu"),
+            })
+        } else {
+            return Option::Some(Food{
+                name:String::from("Strip Steak Sishu"),
+            })
+        }
+    }
+
+    fn deliver_burger(&self, address:&str) ->Result<Food,String> {
+        if self.has_mice_infestation {
+            return Result::Err(String::from("Sorry we have mice infestation"));
+        }
+        
+        if address.is_empty() {
+            return Result::Err(String::from("No delivery address specified"));
+        } else {
+            return Result::Ok(Food{
+                name:String::from("Burger"),
+            });
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, understanifn this options and other add hoc functionalities of Rust!");
+    let zing1 = Restaurant{
+        reservations:11,
+        has_mice_infestation:true,
+    };
+
+    println!("{:?}", zing1.chef_special());
+    println!("{:?}", zing1.deliver_burger("123 Elm Street"));
+
+
+     let zing2 = Restaurant{
+        reservations:15,
+        has_mice_infestation:false,
+    };
+
+    println!("{:?}", zing2.chef_special());
+    println!("{:?}", zing2.deliver_burger(""));
+    println!("{:?}", zing2.deliver_burger("123 Main Street"));
+}
